6ae8c02f01079003f35bdce593b92b007cb11084
SeungJin Jang <sinjin@stronghold.kr>
2018-07-01 23:53:25 -0700

    버튼 스타일 적용 위치 변경

--- INDIVISUAL/SINJIN/WEB/todos_ui/src/components/Link.js
+++ INDIVISUAL/SINJIN/WEB/todos_ui/src/components/Link.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 
 const Link = ({ active, children, onClick }) => {
   if (active) {
-    return <span className="btn btn-block btn-lg btn-secondary">{children}</span>
+    return <span className="btn btn-xs btn-info">{children}</span>
   }
 
   return (
     // eslint-disable-next-line
     <button type="button"
-      className="btn btn-block btn-lg btn-outline-secondary"
+      className="btn btn-xs btn-outline-info"
       onClick={e => {
         e.preventDefault()
         onClick()
